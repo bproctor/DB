6b14cb58a04d448b7b5af17670d2c3a023b6d3e8
Brad Proctor <brad@bradleyproctor.com>
2012-01-09 11:00:53 -0500

    Bump to version 1.1

--- db.php
+++ db.php
@@ -12,7 +12,7 @@
  * @copyright   Copyright (c) 2010-2012 Brad Proctor
  * @license     MIT License (http://www.opensource.org/licenses/mit-license.php)
  * @link        http://bradleyproctor.com/
- * @version     1.0
+ * @version     1.1
  */
 class Database_Exception extends \FuelException {}
 
